bc2aa20129a3e49bee8f11975897af0718460a96
Jonathan <78237375+jonathanduran7@users.noreply.github.com>
2022-01-15 12:57:40 -0300

    added typewriter

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Typewriter from 'typewriter-effect';
 import "./home.css"
 
 const Home = () => {
+      
       return (
             <div className='home'>
                   <h1>Welcome!</h1>
-                  <p className='text-p'>My name is Jonathan Duran</p>
+                  <div className='text-p'>
+                  <Typewriter 
+                        options={{
+                              strings: ['My name is Jonathan', "I'm programming"],
+                              autoStart: true,
+                              loop: true
+                        }}
+                  />
+                  </div>
                   <div className="cards">
                         <div className="card-box">
                               <Link className='link'  to="projects">Projects</Link>
