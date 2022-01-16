759a3b946be18bbf49574a86e599da9ae70b375a
Jonathan <78237375+jonathanduran7@users.noreply.github.com>
2022-01-16 10:33:23 -0300

    change background

---
+++ src/ParticleBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Particles from 'react-tsparticles'
+import particlesConfig from './config/particle-config'
+
+const ParticleBackground = () => {
+      return (
+            <Particles params={particlesConfig}></Particles>
+      )
+}
+
+export default ParticleBackground
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Typewriter from 'typewriter-effect';
+import ParticleBackground from '../../ParticleBackground';
 import "./home.css"
 
 const Home = () => {
-      
+           
       return (
             <div className='home'>
-                  <h1>Welcome!</h1>
-                  <div className='text-p'>
-                  <Typewriter 
-                        options={{
-                              strings: ['My name is Jonathan', "I'm programming"],
-                              autoStart: true,
-                              loop: true
-                        }}
-                  />
+                  <div className='particleBack'>
+                        <ParticleBackground/>
                   </div>
-                  <div className="cards">
-                        <div className="card-box">
-                              <Link className='link'  to="projects">Projects</Link>
+
+                  <div className='home-content'>
+                        <h1>Welcome!</h1>
+                        <div className='text-p'>
+                        <Typewriter 
+                              options={{
+                                    strings: ['My name is Jonathan', "I'm programming"],
+                                    autoStart: true,
+                                    loop: true
+                              }}
+                              />
                         </div>
-                        <div className="card-box">
-                              <Link className='link'  to="about">About</Link>
-                        </div>
-                        <div className="card-box">
-                              <Link className='link'  to="contact">Contact</Link>
+                        <div className="cards">
+                              <div className="card-box">
+                                    <Link className='link'  to="projects">Projects</Link>
+                              </div>
+                              <div className="card-box">
+                                    <Link className='link'  to="about">About</Link>
+                              </div>
+                              <div className="card-box">
+                                    <Link className='link'  to="contact">Contact</Link>
+                              </div>
                         </div>
                   </div>
             </div>
